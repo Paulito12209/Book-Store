231c48c600653d2d27c003477af9a45af44410dc
Son Paul <paul.angeles.chaquire@gmail.com>
2025-09-10 01:04:56 +0200

    upl files

---
+++ script.js
@@ -0,0 +1,12 @@
+// === Objects ===
+let books = {
+'title' : 'Die Geheimnisse des Ozeans'
+}
+
+// === Initial FUNKTION ===
+function init() {
+    renderBooks ();
+}
+
+// === RENDER FUNKTIONEN ===
+function renderBooks() {}
